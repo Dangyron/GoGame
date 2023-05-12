01083c1f5702e0d78cee4f7b79dba90291b3b329
Dangyron <78tuerpocv01@gmail.com>
2023-05-12 17:00:52 +0200

    Move board realization in Board class

--- GoGameApp/Board.cs
+++ GoGameApp/Board.cs
@@ -1,6 +1,75 @@
-﻿namespace GoGameApp;
+﻿using System.Windows.Controls;
+using System.Windows.Shapes;
+using GoGame.Utility;
+
+namespace GoGameApp;
 
 public class Board
 {
+    private readonly Canvas _boardCanvas;
+
+    public Board(Canvas boardCanvas)
+    {
+        _boardCanvas = boardCanvas;
+    }
+
+    public void Draw()
+    {
+        DrawBoardBorders();
+        DrawBoardStars();
+    }
+    private void DrawBoardBorders()
+    {
+        _boardCanvas.Background = Constants.BoardBackGroundColour;
+        for (var i = 0; i < Constants.CountOfCells; i++)
+        {
+            var verticalLine = new Line
+            {
+                X1 = i * Constants.CellSize + Constants.BoardHorizontalMargin,
+                Y1 = Constants.BoardVerticalMargin,
+                X2 = i * Constants.CellSize + Constants.BoardHorizontalMargin,
+                Y2 = Constants.GridLineLength + Constants.BoardVerticalMargin,
+                Stroke = Constants.BoardStrokeColour,
+                StrokeThickness = Constants.BoardStrokeThickness
+            };
+            _boardCanvas.Children.Add(verticalLine);
+
+            var horizontalLine = new Line
+            {
+                X1 = Constants.BoardHorizontalMargin,
+                Y1 = i * Constants.CellSize + Constants.BoardVerticalMargin,
+                X2 = Constants.GridLineLength + Constants.BoardHorizontalMargin,
+                Y2 = i * Constants.CellSize + Constants.BoardVerticalMargin,
+                Stroke = Constants.BoardStrokeColour,
+                StrokeThickness = Constants.BoardStrokeThickness
+            };
+            _boardCanvas.Children.Add(horizontalLine);
+        }
+    }
     
+    private void DrawBoardStars()
+    {
+        var starPoint = new Ellipse
+        {
+            Width = Constants.BoardStarSize,
+            Height = Constants.BoardStarSize,
+            Fill = Constants.BoardStrokeColour
+        };
+
+        for (var i = 0; i < 3; i++)
+        {
+            for (var j = 0; j < 3; j++)
+            {
+                Canvas.SetLeft(starPoint, 3 * Constants.CellSize * (1 + j * 2) + Constants.BoardHorizontalMargin - Constants.BoardStarSize / 2.0);
+                Canvas.SetTop(starPoint, 3 * Constants.CellSize * (1 + i * 2) + + Constants.BoardVerticalMargin - Constants.BoardStarSize / 2.0);
+                _boardCanvas.Children.Add(starPoint);
+                starPoint = new Ellipse
+                {
+                    Width = 10,
+                    Height = 10,
+                    Fill = Constants.BoardStrokeColour
+                };
+            }
+        }
+    }
 }
--- GoGameApp/MainWindow.xaml.cs
+++ GoGameApp/MainWindow.xaml.cs
@@ -6,68 +6,13 @@ namespace GoGameApp;
 
 public partial class MainWindow
 {
+    private readonly Board _board;
     public MainWindow()
     {
         InitializeComponent();
+        _board = new Board(BoardCanvas);
         Width = Constants.WindowWidth;
         Height = Constants.WindowHeight;
-        DrawBoard();
-    }
-
-    private void DrawBoard()
-    {
-        BoardCanvas.Background = Constants.BoardBackGroundColour;
-        for (var i = 0; i < Constants.CountOfCells; i++)
-        {
-            var verticalLine = new Line
-            {
-                X1 = i * Constants.CellSize + Constants.BoardHorizontalMargin,
-                Y1 = Constants.BoardVerticalMargin,
-                X2 = i * Constants.CellSize + Constants.BoardHorizontalMargin,
-                Y2 = Constants.GridLineLength + Constants.BoardVerticalMargin,
-                Stroke = Constants.BoardStrokeColour,
-                StrokeThickness = Constants.BoardStrokeThickness
-            };
-            BoardCanvas.Children.Add(verticalLine);
-
-            var horizontalLine = new Line
-            {
-                X1 = Constants.BoardHorizontalMargin,
-                Y1 = i * Constants.CellSize + Constants.BoardVerticalMargin,
-                X2 = Constants.GridLineLength + Constants.BoardHorizontalMargin,
-                Y2 = i * Constants.CellSize + Constants.BoardVerticalMargin,
-                Stroke = Constants.BoardStrokeColour,
-                StrokeThickness = Constants.BoardStrokeThickness
-            };
-            BoardCanvas.Children.Add(horizontalLine);
-        }
-
-        DrawBoardStars();
-    }
-    
-    private void DrawBoardStars()
-    {
-        var starPoint = new Ellipse
-        {
-            Width = Constants.BoardStarSize,
-            Height = Constants.BoardStarSize,
-            Fill = Constants.BoardStrokeColour
-        };
-
-        for (var i = 0; i < 3; i++)
-        {
-            for (var j = 0; j < 3; j++)
-            {
-                Canvas.SetLeft(starPoint, 3 * Constants.CellSize * (1 + j * 2) + Constants.BoardHorizontalMargin - Constants.BoardStarSize / 2.0);
-                Canvas.SetTop(starPoint, 3 * Constants.CellSize * (1 + i * 2) + + Constants.BoardVerticalMargin - Constants.BoardStarSize / 2.0);
-                BoardCanvas.Children.Add(starPoint);
-                starPoint = new Ellipse
-                {
-                    Width = 10,
-                    Height = 10,
-                    Fill = Constants.BoardStrokeColour
-                };
-            }
-        }
+        _board.Draw();
     }
 }
