165d0681164abc9e7f4141a000d13d55ee387643
Dangyron <78tuerpocv01@gmail.com>
2023-05-26 12:19:51 +0200

    Created class SnonesList

--- GoGame.Utility/Models/Board.cs
+++ GoGame.Utility/Models/Board.cs
@@ -1,44 +1,30 @@
-﻿using System.Windows.Shapes;
-using GoGame.Utility;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using GoGame.Utility.Helpers;
 
-namespace GoGameApp.Models;
+namespace GoGame.Utility.Models;
 
 public class Board
 {
-    private readonly List<List<Stone>> _prevStones;
-    private readonly List<List<Stone>> _stones;
+    private readonly StonesList _prevStones;
+    private readonly StonesList _stones;
     public Canvas BoardCanvas { get; }
     public Board(Canvas boardCanvas)
     {
         BoardCanvas = boardCanvas;
-        _stones = new List<List<Stone>>(Constants.CountOfCells);
-        _prevStones = new List<List<Stone>>(Constants.CountOfCells);
-        InitPoints();
+        _stones = new ();
+        _prevStones = new ();
     }
 
     public bool ContainsStoneAtThisPosition(Point position)
     {
         var indexer = position.ConvertPositionToIndexers();
 
-        if (indexer == Constants.UndefinedIndexer)
+        if (indexer.Equals(Constants.Constants.UndefinedIndexer))
             return false;
         
-        return _stones[indexer.I][indexer.J].StoneStates != StonesStates.Empty;
-    }
-    
-    private void InitPoints()
-    {
-        for (var i = 0; i < Constants.CountOfCells; i++)
-        {
-            _stones.Add(new List<Stone>(Constants.CountOfCells));
-            _prevStones.Add(new List<Stone>(Constants.CountOfCells));
-            
-            for (var j = 0; j < Constants.CountOfCells; j++)
-            {
-                _stones[i].Add(new Stone(StonesStates.Empty));
-                _prevStones[i].Add(new Stone(StonesStates.Empty));
-            }
-        }
+        return _stones[indexer].StoneStates != StonesStates.Empty;
     }
 
     public void Draw()
@@ -50,14 +36,14 @@ public class Board
     
     private void DrawStones()
     {
-        for (int i = 0; i < Constants.CountOfCells; i++)
+        for (int i = 0; i < Constants.Constants.CountOfCells; i++)
         {
-            for (int j = 0; j < Constants.CountOfCells; j++)
+            for (int j = 0; j < Constants.Constants.CountOfCells; j++)
             {
                 var position = new Point
                 {
-                    X = Constants.BoardHorizontalMargin + j * Constants.CellSize - Constants.StoneSize / 2.0,
-                    Y = Constants.BoardVerticalMargin + i * Constants.CellSize - Constants.StoneSize / 2.0
+                    X = Constants.Constants.BoardHorizontalMargin + j * Constants.Constants.CellSize - Constants.Constants.StoneSize / 2.0,
+                    Y = Constants.Constants.BoardVerticalMargin + i * Constants.Constants.CellSize - Constants.Constants.StoneSize / 2.0
                 };
                 _stones[i][j].Imagination.UpdateStoneSize();
 
@@ -71,55 +57,41 @@ public class Board
     public bool AddStone(Stone stone, Point point)
     {
         var position = point.ConvertPositionToIndexers();
-
-        if (position.Equals(Constants.UndefinedIndexer))
-        {
-            return false;
-        }
         
-        if (_stones[position.I][position.J].StoneStates == StonesStates.Empty)
+        if (_stones.CheckAllRules(stone, position, _prevStones))
         {
-            var tmp = _stones[position.I][position.J];
             _stones[position.I][position.J] = stone;
-
-            if (_stones == _prevStones)
-            {
-                _stones[position.I][position.J] = tmp;
-                Notification.PositionRepetition();
-                return false;
-            }
 
             _prevStones[position.I][position.J] = stone;
             return true;
         }
-        Notification.InvalidMove();
 
         return false;
     }
 
     private void DrawBoardBorders()
     {
-        for (var i = 0; i < Constants.CountOfCells; i++)
+        for (var i = 0; i < Constants.Constants.CountOfCells; i++)
         {
             var verticalLine = new Line
             {
-                X1 = i * Constants.CellSize + Constants.BoardHorizontalMargin,
-                Y1 = Constants.BoardVerticalMargin,
-                X2 = i * Constants.CellSize + Constants.BoardHorizontalMargin,
-                Y2 = Constants.GridLineLength + Constants.BoardVerticalMargin,
-                Stroke = Constants.BoardStrokeColour,
-                StrokeThickness = Constants.BoardStrokeThickness
+                X1 = i * Constants.Constants.CellSize + Constants.Constants.BoardHorizontalMargin,
+                Y1 = Constants.Constants.BoardVerticalMargin,
+                X2 = i * Constants.Constants.CellSize + Constants.Constants.BoardHorizontalMargin,
+                Y2 = Constants.Constants.GridLineLength + Constants.Constants.BoardVerticalMargin,
+                Stroke = Constants.Constants.BoardStrokeColour,
+                StrokeThickness = Constants.Constants.BoardStrokeThickness
             };
             BoardCanvas.Children.Add(verticalLine);
 
             var horizontalLine = new Line
             {
-                X1 = Constants.BoardHorizontalMargin,
-                Y1 = i * Constants.CellSize + Constants.BoardVerticalMargin,
-                X2 = Constants.GridLineLength + Constants.BoardHorizontalMargin,
-                Y2 = i * Constants.CellSize + Constants.BoardVerticalMargin,
-                Stroke = Constants.BoardStrokeColour,
-                StrokeThickness = Constants.BoardStrokeThickness
+                X1 = Constants.Constants.BoardHorizontalMargin,
+                Y1 = i * Constants.Constants.CellSize + Constants.Constants.BoardVerticalMargin,
+                X2 = Constants.Constants.GridLineLength + Constants.Constants.BoardHorizontalMargin,
+                Y2 = i * Constants.Constants.CellSize + Constants.Constants.BoardVerticalMargin,
+                Stroke = Constants.Constants.BoardStrokeColour,
+                StrokeThickness = Constants.Constants.BoardStrokeThickness
             };
             BoardCanvas.Children.Add(horizontalLine);
         }
@@ -129,9 +101,9 @@ public class Board
     {
         var starPoint = new Ellipse
         {
-            Width = Constants.BoardStarSize,
-            Height = Constants.BoardStarSize,
-            Fill = Constants.BoardStrokeColour
+            Width = Constants.Constants.BoardStarSize,
+            Height = Constants.Constants.BoardStarSize,
+            Fill = Constants.Constants.BoardStrokeColour
         };
 
         for (var i = 0; i < 3; i++)
@@ -139,17 +111,17 @@ public class Board
             for (var j = 0; j < 3; j++)
             {
                 Canvas.SetLeft(starPoint,
-                    3 * Constants.CellSize * (1 + j * 2) + Constants.BoardHorizontalMargin -
-                    Constants.BoardStarSize / 2.0);
+                    3 * Constants.Constants.CellSize * (1 + j * 2) + Constants.Constants.BoardHorizontalMargin -
+                    Constants.Constants.BoardStarSize / 2.0);
                 Canvas.SetTop(starPoint,
-                    3 * Constants.CellSize * (1 + i * 2) + Constants.BoardVerticalMargin -
-                    Constants.BoardStarSize / 2.0);
+                    3 * Constants.Constants.CellSize * (1 + i * 2) + Constants.Constants.BoardVerticalMargin -
+                    Constants.Constants.BoardStarSize / 2.0);
                 BoardCanvas.Children.Add(starPoint);
                 starPoint = new Ellipse
                 {
-                    Width = Constants.BoardStarSize,
-                    Height = Constants.BoardStarSize,
-                    Fill = Constants.BoardStrokeColour
+                    Width = Constants.Constants.BoardStarSize,
+                    Height = Constants.Constants.BoardStarSize,
+                    Fill = Constants.Constants.BoardStrokeColour
                 };
             }
         }
---
+++ GoGame.Utility/Models/StonesList.cs
@@ -0,0 +1,70 @@
+﻿namespace GoGame.Utility.Models;
+
+public class StonesList
+{
+    private readonly List<List<Stone>> _stones;
+
+    public StonesList()
+    {
+        _stones = new List<List<Stone>>(Constants.Constants.CountOfCells);
+        InitStones();
+    }
+
+    public Stone this[StoneIndexer index] => _stones[index.I][index.J];
+
+    public List<Stone> this[int i] => _stones[i];
+
+    public int Count => _stones.Count;
+
+    public void Place(StoneIndexer index, Stone stone)
+    {
+        _stones[index.I][index.J] = stone;
+    }
+    
+    public void Remove(StoneIndexer position)
+    {
+        _stones[position.I][position.J] = new Stone(StonesStates.Empty);
+    }
+    
+    private void InitStones()
+    {
+        for (var i = 0; i < Constants.Constants.CountOfCells; i++)
+        {
+            _stones.Add(new List<Stone>(Constants.Constants.CountOfCells));
+            
+            for (var j = 0; j < Constants.Constants.CountOfCells; j++)
+            {
+                _stones[i].Add(new Stone(StonesStates.Empty));
+            }
+        }
+    }
+    
+    public static bool operator ==(StonesList a, StonesList b)
+    {
+        if (ReferenceEquals(a, b))
+            return true;
+
+        if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+            return false;
+
+        return a._stones.SequenceEqual(b._stones);
+    }
+
+    public static bool operator !=(StonesList a, StonesList b)
+    {
+        return !(a == b);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(obj, null) || GetType() != obj.GetType())
+            return false;
+
+        return this == (StonesList)obj;
+    }
+
+    public override int GetHashCode()
+    {
+        return _stones.GetHashCode();
+    }
+}
